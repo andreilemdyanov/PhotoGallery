0ef6de7a6d2924eea902b7e933ec850f46b96988
andreilemdyanov <lemdyanov5@mail.ru>
2020-05-08 22:39:02 +0300

    HttpUrlConnection.

--- app/src/main/java/com/work/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/work/photogallery/PhotoGalleryFragment.java
@@ -1,11 +1,37 @@
 package com.work.photogallery;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
 
     public static Fragment newInstance() {
         return new PhotoGalleryFragment();
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mRecyclerView = v.findViewById(R.id.photo_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        return v;
+    }
+
 }
