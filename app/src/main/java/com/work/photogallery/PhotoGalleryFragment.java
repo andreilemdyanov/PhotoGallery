21bd66a1486cb92d0e2474629804dd2f0728523b
andreilemdyanov <lemdyanov5@mail.ru>
2020-05-08 22:14:39 +0300

    Initial commit

---
+++ app/src/main/java/com/work/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,11 @@
+package com.work.photogallery;
+
+import androidx.fragment.app.Fragment;
+
+class PhotoGalleryFragment extends Fragment {
+
+
+    public static Fragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+}
